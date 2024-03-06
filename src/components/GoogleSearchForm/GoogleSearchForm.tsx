4767df7aba5f8a2726a1842aba5930744d311e52
tsunacan <sin2016iba@gmail.com>
2024-03-06 22:29:40 +0900

    add search form

---
+++ src/components/GoogleSearchForm/GoogleSearchForm.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const GoogleSearchForm: React.FC = () => {
+  function search(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const query = formData.get("query");
+    document.location.href = `https://www.google.com/search?q=${query}`; // TODO use encodeURIComponent
+  }
+
+  return (
+    <div>
+      <form onSubmit={search}>
+        <input name="query" placeholder="Search Google" />
+      </form>
+    </div>
+  );
+};
+
+export default GoogleSearchForm;
