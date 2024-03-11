96bdf291df1c0323b75df983fc51f33533e22113
tsunacan <sin2016iba@gmail.com>
2024-03-12 08:44:48 +0900

    add ci

--- src/components/GoogleSearchForm/GoogleSearchForm.tsx
+++ src/components/GoogleSearchForm/GoogleSearchForm.tsx
@@ -1,21 +1,21 @@
-import React from "react";
+import type React from "react";
 import "./GoogleSearchForm.scss";
 
 const GoogleSearchForm: React.FC = () => {
-  function search(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const query = formData.get("query");
-    document.location.href = `https://www.google.com/search?q=${query}`; // TODO use encodeURIComponent
-  }
+	function search(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		const formData = new FormData(event.currentTarget);
+		const query = formData.get("query");
+		document.location.href = `https://www.google.com/search?q=${query}`; // TODO use encodeURIComponent
+	}
 
-  return (
-    <div>
-      <form onSubmit={search}>
-        <input name="query" autoFocus={true} placeholder="Search Google" />
-      </form>
-    </div>
-  );
+	return (
+		<div>
+			<form onSubmit={search}>
+				<input name="query" autoFocus={true} placeholder="Search Google" />
+			</form>
+		</div>
+	);
 };
 
 export default GoogleSearchForm;
